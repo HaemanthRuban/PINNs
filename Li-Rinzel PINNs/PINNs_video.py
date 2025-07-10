ad50e6b6244180ba556264d0dec4a9f8a9ce05f6
HaemanthRuban <haemanthruban2021@gmail.com>
2025-07-10 23:44:35 +0530

    added PINNs Video

---
+++ Li-Rinzel PINNs/PINNs_video.py
@@ -0,0 +1,256 @@
+import numpy as np
+import torch 
+import torch.nn as nn
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+
+def lirinzel_ns(v2, a2, IP3=0.5, d5=0.09, k3=0.08, dt=0.01):
+    c0, c1 = 2, 0.185
+    v1, v3 = 6, 0.9
+    d1, d2, d3, Nd = 0.13, 1.049, 0.9434, 2
+    time = np.arange(0, 100, dt)
+    dt = dt
+    Ca, h = [0.1], [0.1]  # Initialize lists
+
+    # ODE solving
+    for _ in range(1, len(time)):  # Start from index 1 to avoid index errors
+        CaER = (c0 - Ca[-1]) / c1
+        pinf = IP3 / (IP3 + d1)
+        ninf = Ca[-1] / (Ca[-1] + d5)
+        Q2 = d2 * (IP3 + d1) / (IP3 + d3)
+        tauh = 1 / (a2 * (Q2 + Ca[-1]))
+        hinf = Q2 / (Q2 + Ca[-1])
+        alphah = hinf / tauh
+        betah = (1 - hinf) / tauh
+        dh = alphah * (1 - h[-1]) - betah * h[-1]
+        dCa = (c1 * v1 * pinf**3 * ninf**3 * h[-1]**3 * (CaER - Ca[-1]) +
+               c1 * v2 * (CaER - Ca[-1]) -
+               v3 * Ca[-1]**2 / (Ca[-1]**2 + k3**2))
+
+        # Append new values instead of assigning to an index
+        Ca.append(Ca[-1] + dCa * dt)
+        h.append(h[-1] + dh * dt)
+
+    # Convert to NumPy array for interpolation
+    Ca = np.array(Ca)
+    h = np.array(h)
+
+    return Ca, h
+
+v2 = 0.1
+a2 = 0.15
+IP3 = 0.5
+d5 = 0.09
+k3  = 0.08
+
+dt1 = 0.01
+t1 = np.arange(0, 100, dt1)
+Ca_euler1, h_euler1 = lirinzel_ns(v2, a2, IP3, d5, k3, dt1)
+
+class FourierFeatureLayer(nn.Module):
+    def __init__(self, num_features=1):
+        super(FourierFeatureLayer, self).__init__()
+        self.num_features = num_features
+        mean = 0.0
+        std = 1.0
+        size = (num_features, 1)
+        self.sine_coeff = nn.Parameter(torch.normal(mean, std, size=size))   
+        self.cosine_coeff = nn.Parameter(torch.normal(mean, std, size=size))
+
+    def forward(self, t): 
+        sine_feats = torch.sin(torch.matmul(t, self.sine_coeff.T))    
+        cosine_feats = torch.cos(torch.matmul(t, self.cosine_coeff.T))
+        feat = torch.cat([sine_feats, cosine_feats], dim=1)
+        return feat
+
+class PinnModel(nn.Module):
+    def __init__(self, hidden_units, num_features, y0):
+        super().__init__()
+        self.y0 = y0
+        self.feature = FourierFeatureLayer(num_features=num_features)
+        self.layer1 = nn.Linear(in_features=2*num_features, out_features=hidden_units)
+        self.tanh = nn.Tanh()
+        self.layer2 = nn.Linear(in_features=hidden_units, out_features=hidden_units)
+        self.layer3 = nn.Linear(in_features=hidden_units, out_features=hidden_units)
+        self.layer4 = nn.Linear(in_features=hidden_units, out_features=2)
+
+    def forward(self, x):
+        out = self.layer4(self.tanh(self.layer3(self.tanh(self.layer2(self.tanh(self.layer1(self.feature(x))))))))
+        scaled = self.y0 + self.tanh(x) * torch.tensor([1, 1]) * out
+        return scaled
+
+Ca_euler = torch.tensor(Ca_euler1, dtype=torch.float32)[::100]
+h_euler = torch.tensor(h_euler1, dtype=torch.float32)[::100]
+
+torch.manual_seed(2)
+pinn = PinnModel(hidden_units=30, num_features=3, y0=torch.tensor([0.1, 0.1]).view(1,-1))
+
+t_test = torch.arange(0, 100, 0.01).view(-1, 1)
+time = torch.arange(0, 100, 1.0).view(-1, 1)
+t_initial = torch.tensor(0.0, requires_grad=True).view(-1, 1)
+t_physics = torch.linspace(0, 100, 200, requires_grad=True).view(-1, 1)
+
+c0, c1 = 2, 0.185
+v1, v3 = 6, 0.9
+d1, d2, d3, Nd = 0.13, 1.049, 0.9434, 2
+IP3 = 0.5
+d5 = 0.09
+k3  = 0.08
+pinf = IP3 / (IP3 + d1)
+Q2 = d2 * (IP3 + d1) / (IP3 + d3)
+v2_true = 0.1
+a2_true = 0.15
+
+v2 = torch.nn.Parameter(torch.rand(1, requires_grad=True))
+a2 = torch.nn.Parameter(torch.rand(1, requires_grad=True))
+lam1, lam2, lam3 = 1e-2, 1, 1e-2
+optimiser = torch.optim.Adam(list(pinn.parameters()) + [v2, a2], lr=0.001)
+epochs =  10001
+v2s = []
+a2s = []
+Ca_predict = []
+Ca_euler_pred = []
+
+for i in range(epochs):
+
+    optimiser.zero_grad()
+
+    pinn.train()
+
+    # Data loss
+    sol = pinn(time)
+    Ca_preds = sol[:, 0].view(-1, 1)
+    h_preds = sol[:, 1].view(-1, 1)
+    loss1 = torch.mean((Ca_preds.squeeze() - Ca_euler)**2)
+    loss2 = torch.mean((h_preds.squeeze() - h_euler)**2)
+
+    # Initial loss
+    Ca0, h0 = pinn(t_initial)[0]
+    loss3 = (torch.squeeze(Ca0) - 0.1)**2
+    loss4 = (torch.squeeze(h0) - 0.1)**2
+
+    # Physics loss
+    Ca = pinn(t_physics)[:, 0].view(-1, 1)
+    dCa_dt = torch.autograd.grad(Ca, t_physics, torch.ones_like(Ca), create_graph=True, retain_graph=True)[0]
+
+    h = pinn(t_physics)[:, 1].view(-1, 1)
+    dh_dt = torch.autograd.grad(h, t_physics, torch.ones_like(h), create_graph=True, retain_graph=True)[0]
+
+    CaER = (c0 - Ca) / c1
+    ninf = Ca / (Ca + d5)
+    tauh = 1 / (a2 * (Q2 + Ca))
+    hinf = Q2 / (Q2 + Ca)
+    alphah = hinf / tauh
+    betah = (1 - hinf) / tauh
+
+    res1 = dCa_dt - (c1*v1*(pinf**3)*(ninf**3)*(h**3)*(CaER - Ca) + c1*v2*(CaER-Ca) - v3*(Ca**2 / (Ca**2 + k3**2)))
+    res2 = dh_dt - (alphah*(1-h) - betah*h)
+    loss5 = torch.mean((res1)**2)
+    loss6 = torch.mean((res2)**2)
+
+    # Total loss
+    loss = lam1*(loss1 + loss2) + lam2*(loss3 + loss4) + lam3*(loss5 + loss6)
+    loss.backward()
+    optimiser.step()
+
+    v2s.append(v2.item())
+    a2s.append(a2.item())
+
+    if i % 100 == 0:
+        print(f"Training step: {i}")
+        print(f"Loss = {loss} | Physics_loss = {loss5 + loss6}| Initial_loss = {loss3 + loss4} | Data_loss = {loss1 + loss2}")
+
+        pinn.eval()
+        with torch.inference_mode():
+          Ca_pred, _ = [pinn(t_test)[:,i] for i in range(2)]
+        
+        Ca_predict.append(np.array(Ca_pred.detach().squeeze()))
+        Ca_euler_pred.append(lirinzel_ns(v2s[-1], a2s[-1])[0])
+
+        '''plt.figure(figsize=(8, 6))
+        plt.title(f"Training step: {i}")
+
+        # plt.subplot(221)
+        plt.plot(time.squeeze().cpu(), Ca_euler.detach().cpu(), '.g', label="Observational Data")
+        plt.plot(t_test.squeeze().cpu(), Ca_pred.detach().cpu(), label="PINN Predictions")
+        plt.title("Ca prediction")
+        plt.xlabel("$t$")
+        plt.ylabel("$Ca$")
+        plt.legend()
+
+        plt.subplot(222)
+        plt.plot(time.squeeze().cpu(), h_euler.detach().cpu(), '.g', label="Observational Data")
+        plt.plot(t_test.squeeze().cpu(), h_pred.detach().cpu(), label="PINN Predictions")
+        plt.title("h prediction")
+        plt.xlabel("$t$")
+        plt.ylabel("$h$")
+        plt.legend()
+
+        plt.subplot(223)
+        plt.plot(v2s, label="PINN estimate", c='green')
+        plt.hlines(0.1, 0, len(v2s), color="tab:grey", label="True Value")
+        plt.title("v2-value estimation")
+        plt.xlabel("Training step")
+        plt.legend()
+
+        plt.subplot(224)
+        plt.plot(a2s, label="PINN estimate", c='green')
+        plt.hlines(0.15, 0, len(a2s), color="tab:grey", label="True Value")
+        plt.title("a2-value estimation")
+        plt.xlabel("Training step")
+        plt.legend()
+        plt.tight_layout()
+        plt.show()'''
+
+Ca_predict = np.array(Ca_predict)
+Ca_euler_pred = np.array(Ca_euler_pred)
+
+fig, axes = plt.subplots(1, 2, figsize=(14, 3))
+
+ax = axes[0]
+anim_plot1, = ax.plot([], [], label="PINN Prediction")
+ax.plot(time, Ca_euler, '.g', label="Observational Data")
+ax.set_title("Ca PINN Prediction")
+ax.set_xlabel("Time")
+ax.set_ylabel("$Ca$")
+ax.legend()
+
+ax = axes[1]
+anim_plot2, = ax.plot([], [], label="Euler Prediction")
+ax.plot(time, Ca_euler, '.g', label="Observational Data")
+ax.set_title("Ca Euler Prediction")
+ax.set_xlabel("Time")
+ax.set_ylabel("$Ca$")
+ax.legend()
+
+'''ax = axes[1,0]
+anim_plot3, = ax.plot([], [], c='green')
+ax.hlines(v2_true, 0, epochs, color="tab:grey", label="True Value")
+ax.set_title("v2-value estimation")
+ax.set_xlabel("Training Step")
+ax.set_ylabel("Value")
+ax.legend()
+
+ax = axes[1,1]
+anim_plot4, = ax.plot([],[], c='green')
+ax.hlines(a2_true, 0, epochs, color="tab:grey", label="True Value")
+ax.set_title("a2-value estimation")
+ax.set_xlabel("Training Step")
+ax.set_ylabel("Value")
+ax.legend()'''
+
+def animate(i):
+    plt.suptitle(f"Epoch = {100*(i+1)}")
+    anim_plot1.set_data(t_test.squeeze(), Ca_predict[i])
+    anim_plot2.set_data(t_test.squeeze(), Ca_euler_pred[i])
+    # x_pred_plot = np.linspace(0, epochs, 1)[::100]
+    # anim_plot3.set_data(x_pred_plot[:i], v2s[::100][:i])
+    # anim_plot4.set_data(x_pred_plot[:i], a2s[::100][:i])
+    return anim_plot1, anim_plot2, 
+
+plt.tight_layout()
+
+animation = FuncAnimation(fig, animate, frames=100, interval=0.1)
+animation.save("Li-Rinzel PINNs/Videos/time_100_PINN.gif")
+
+
